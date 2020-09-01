69ea3b540b0e8e68c9634bcaf92bfac8a1279412
Proger-Maxwell <70543498+Proger-Maxwell@users.noreply.github.com>
2020-09-01 22:55:04 +0300

    Initial project version

---
+++ application/views/404_view.php
@@ -0,0 +1,6 @@
+<title>HandBook 404</title>
+</head>
+<h1>404</h1>
+<p>
+    404
+</p>
---
+++ application/views/main_view.php
@@ -0,0 +1,11 @@
+<body>
+<h1>Main HandBook</h1>
+<p>
+       Main page "p"
+</p>
+<div class="row">
+    <div class="col-sm-3">.col-sm-3</div>
+    <div class="col-sm-6">.col-sm-6</div>
+    <div class="col-sm-3">.col-sm-3</div>
+</div>
+</body>
